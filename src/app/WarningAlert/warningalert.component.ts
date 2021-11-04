176ecc1b0a53f75f0696bc362e4c75011c00b630
Raul Crespo <raul@raulcrespo.tech>
2021-11-04 19:15:15 -0300

    first task of the course

---
+++ src/app/WarningAlert/warningalert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-WarningAlert',
+    templateUrl: './warningalert.component.html',
+    styleUrls: ['./warningalert.component.css']
+})
+
+export class WarningComponent { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //styles: ['h3{color:blue;}']
 })
 export class AppComponent {
-  title = 'angular-udemy';
 }
