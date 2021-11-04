8664706781c08e51483cad43ad0c60acddda2412
Raul Crespo <raul@raulcrespo.tech>
2021-11-04 19:34:23 -0300

    deletd task files so i can keep up with the course

--- src/app/WarningAlert/warningalert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-WarningAlert',
-    templateUrl: './warningalert.component.html',
-    styleUrls: ['./warningalert.component.css']
-})
-
-export class WarningComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningComponent } from './WarningAlert/warningalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
